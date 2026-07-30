f4834ba01374bf3315f3265f280f2a58d6be8c39
baty03 <baktygulzvezda@gmail.com>
2026-07-30 14:00:37 +0600

    update

--- news/views.py
+++ news/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Book
 
+from django.core.paginator import Paginator
 
 def home(request):
     books = Book.objects.all()
-    return render(request, 'index.html', {'books': books})
 
+    paginator = Paginator(books, 6)   # по 6 книг на странице
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'index.html', {
+        'page_obj': page_obj
+    })
 
 def book_detail(request, id):
     book = get_object_or_404(Book, id=id)
@@ -13,4 +21,6 @@ def book_detail(request, id):
     return render(request, 'book_detail.html', {
         'book': book
     })
+
+
 # Create your views here.
